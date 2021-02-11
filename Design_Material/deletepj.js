da6364e6892e57139bb1ca4bc2da1b5395dae536
PoonkasemK <poonkasem.kase@gmail.com>
2021-02-11 17:41:00 +0700

    DeleteProjectUpdate

--- Design_Material/deletepj.js
+++ Design_Material/deletepj.js
@@ -1,3 +1,25 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+
+    var DeleteProjectButton = document.getElementsByClassName('DeleteProjectButton')
+    for (var i = 0; i < DeleteProjectButton.length; i++) {
+        var button = DeleteProjectButton[i]
+        button.addEventListener('click', DeleteProject)
+    }
+
+}
+
+function DeleteProject() {
+    var projectToDelete = document.getElementById("SelectProjectToDelete").value;
+    console.log(projectToDelete)
+
+}
+
 //For Project Management -- JN
 
 const express = require('express')

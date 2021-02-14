7748cd4765cc75b90a9a34de1eba5e8e855c7769
chanikanchnt <76995955+chanikanchnt@users.noreply.github.com>
2021-02-14 16:20:03 +0700

    Can delete pj
    Need to set the variable that recieve in to be project id

--- Design_Material/deletepj.js
+++ Design_Material/deletepj.js
@@ -17,6 +17,10 @@ function DeleteProject() {
     var projectToDelete = document.getElementById("SelectProjectToDelete").value;
     console.log(projectToDelete)
 
+    var projectToDelete_id = 35; //Demo
+
+    var link = "http://localhost:5000/deletepj_db/" + projectToDelete_id;
     //TODO: Change Link
-    location.replace(" http://localhost:5000/createpj_db/addnw/11-'eleven'") //Redirect sucessfullt
+    //location.replace("http://localhost:5000/createpj_db/addnw/11-'eleven'") //Redirect sucessfully
+    location.replace(link)
 }
